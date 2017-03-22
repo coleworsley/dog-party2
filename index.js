9742c95c7f21bb2f57677f6e6a482dc2ad2dd1f1
coleworsley <cole.s.worsley@gmail.com>
2017-03-22 11:47:57 -0600

    adding entire file

---
+++ index.js
@@ -0,0 +1,8 @@
+// var el = document.querySelector(".question")
+// el.style.background = "white";
+// document.querySelector(".what-are-dogs img").src = "images/dog2-sq.jpg"
+
+// var questionHover = querySelector(".what-are-dogs img").is(":hover")
+// if questionHover == true {
+//   querySelector(".what-are-dogs img").src = "images/dog2-sq.jpg"
+// }
